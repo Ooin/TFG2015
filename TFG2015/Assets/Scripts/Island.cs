ce3617cb9017b81f9cce6f811c6d9b37110fae80
unknown <mangleca7.alumnes@id34462.da.ub.edu>
2015-03-11 13:54:40 +0100

    Primer Commit, probes

---
+++ TFG2015/Assets/Scripts/Island.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Island : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.AddComponent <TerrainCollider> ();
+		gameObject.AddComponent <Terrain> ();
+		TerrainData illa = new TerrainData ();
+
+		illa.size = new Vector3(10, 600, 10);
+		illa.heightmapResolution = 512;
+		illa.baseMapResolution = 1024;
+		illa.SetDetailResolution(1024, 16);
+
+		int heightmapWidth = illa.heightmapWidth;
+		int heightmapHeight = illa.heightmapHeight;
+		TerrainCollider terrainCollider = gameObject.GetComponent<TerrainCollider>();
+		Terrain terrain = gameObject.GetComponent<Terrain>();
+		terrainCollider.terrainData= illa;
+		terrain.terrainData = illa;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
