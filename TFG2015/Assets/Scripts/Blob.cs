28270b9cd909307d61d034d8f4f5105ca8367a2f
unknown <mangleca7.alumnes@id34462.da.ub.edu>
2015-03-16 13:59:56 +0100

    Inacabat

---
+++ TFG2015/Assets/Scripts/Blob.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blob {
+
+	//De moment sera una esfera situada a unes coordenades 2D del pla.
+	//en un futur pot tenir un cert offset vertical per a no fer nomes mitja esfera, sino una mica menys o una mica mes.
+	private float radius {
+		get {
+			return radius;
+		}
+	}
+	private Vector2 coordinates {
+		get {
+			return coordinates;
+		}
+	}
+
+	public Blob(float rad, Vector2 coord){
+		this.radius = rad;
+		this.coordinates = coord;
+	}
+
+}
--- TFG2015/Assets/Scripts/Island.cs
+++ TFG2015/Assets/Scripts/Island.cs
@@ -9,8 +9,8 @@ public class Island : MonoBehaviour {
 		gameObject.AddComponent <Terrain> ();
 		TerrainData illa = new TerrainData ();
 
-		illa.size = new Vector3(10, 128, 10);
-		illa.heightmapResolution = 1024;
+		illa.size = new Vector3(10, 10, 10);
+		illa.heightmapResolution = 100;
 		illa.baseMapResolution = 256;
 		illa.SetDetailResolution(256, 16);
 
@@ -20,19 +20,54 @@ public class Island : MonoBehaviour {
 		Terrain terrain = gameObject.GetComponent<Terrain>();
 		terrainCollider.terrainData= illa;
 		terrain.terrainData = illa;
-		GenerateHeights (terrain, 12.5f);
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+		GenerateHeights (terrain, 100.5f);
 	
 	}
 
+	void GenerateNewMap(Terrain terrain){
+		Blob[] illes = new Blob[2];
+		float radi;
+		Vector2 coord_ini; //coordenades auxiliar per a generar els randoms
+		bool fin = false;
+		float[,] heights = new float[terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight];
+
+		for (int i = 0; i < illes.Length; i++) {
+			radi = Random.Range(1.0, 100.0) / 10;
+			coord_ini.x = Random.Range(0, 256);
+			coord_ini.y = Random.Range(0, 256);
+			
+			illes[i] = new Blob(radi, coord_ini);
+		}
+
+		//////////////////////////////////////////
+		for (int i = 0; i < terrain.terrainData.heightmapWidth; i++)
+		{
+			for (int k = 0; k < terrain.terrainData.heightmapHeight; k++)
+			{
+				heights[i, k] = Mathf.PerlinNoise(
+										((float)i / (float)terrain.terrainData.heightmapWidth) * tileSize,
+										((float)k / (float)terrain.terrainData.heightmapHeight) * tileSize
+													)/10.0f;
+				heights[i,k] = setHeights(illes, i, k);
+			}
+		}
+
+
+	}
+
+	private float[,] setHeights(Blob[] illes, int x, int y){
+
+	}
+	
 	void GenerateHeights(Terrain terrain, float tileSize)
 	{
 		float[,] heights = new float[terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight];
-		
+
+		Debug.Log (terrain.terrainData.heightmapWidth);
+		Debug.Log (terrain.terrainData.heightmapHeight);
+
+		int alt = 1;
+
 		for (int i = 0; i < terrain.terrainData.heightmapWidth; i++)
 		{
 			for (int k = 0; k < terrain.terrainData.heightmapHeight; k++)
@@ -42,6 +77,10 @@ public class Island : MonoBehaviour {
 		}
 		
 		terrain.terrainData.SetHeights(0, 0, heights);
+	}
+
+	// Update is called once per frame
+	void Update () {
 	}
 
 }
